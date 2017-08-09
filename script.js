a412c93d4e958b683f5bffdf00769bf315bd3429
Hack Reactor Student <student@hackreactor.com>
2017-08-09 12:32:19 -0500

    Whatever you want

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
-// 
+//
 $(document).ready(function(){
 // your code goes below
+  $("#word-1").click(function(){
+  // $("word-1").hide(1000)
+    console.log("word-1");
+    $(".word").hide();
 })
+
+  $("h1").click(function(){
+    $("#word-2,#word-6,#word-7,#word-8,#word-11").show();
+  })
+});
